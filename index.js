197b2b72a6d75bbf960990212b3a26bcaa09a0bb
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 01:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// the readmes and the test errors continue to increase in divergence...
+/*
+At the beginning of the day, the deli is empty and is represented by an empty array, like var katzDeliLine = [];. However, you don't need to code the array as a variable, since the test scripts will create it and pass it to the functions you are about to build.
+*/
+// just say "katzDeliLine" is the array name to refer to then! "like" no, actually, exactly that.
+
 function takeANumber(currentQueue,name){
-  var queuePosition = currentQueue.length + 1;
+  var queuePosition
+  
+
+  queuePosition = currentQueue.length + 1;
   return `Welcome, ${name}. You are number ${queuePosition} in line.`
 }
 
