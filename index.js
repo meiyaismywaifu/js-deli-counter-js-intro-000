d67e36419fdb270b56958c9056b292ff06a4aa7b
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 01:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(currentQueue,name){
-
+  var queueposition = currentQueue.length + 1;
+  return `Welcome, ${name}. You are number ${queuePosition} in line.`
 }
 
 function nowServing (currentQueue){
